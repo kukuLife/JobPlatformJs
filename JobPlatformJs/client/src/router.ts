6b00fb05e5477e9066455ffab093f73dc827f3c6
wanfusang1992@gmail.com <33004613Caonima>
2022-09-25 10:35:11 +0900

    change login path

--- JobPlatformJs/client/src/router.ts
+++ JobPlatformJs/client/src/router.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import registe from './views/register.vue';
-import login from './views/login.vue';
+import login from './views/Login.vue';
 
 Vue.use(Router);
 
